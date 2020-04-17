12d74710e56fb4f46c3eaff5555c71508298f371
Edwin Jimenez <ejjimenezc@unal.edu.co>
2020-04-17 14:10:54 -0500

    Added Settings and Log Tabs

--- Nvivo Activator/Activator.py
+++ Nvivo Activator/Activator.py
@@ -10,7 +10,7 @@ import base64
 import os
 
 #Global Variables
-NVIVO = r'C:\Program Files\QSR\NVivo 12\nvivo'
+DEFAULT_NVIVO = r'C:\Program Files\QSR\NVivo 12\nvivo.exe'
 KEY = "9aSa-nUrp21l7I0FQcBHrtinmaiB56G7-ZXzJTNpjj8="
 COUNTRIES = []
 
@@ -18,11 +18,11 @@ with open("AcceptedCountryFormat.txt","r") as cf:
     COUNTRIES = cf.readlines()
 
 #Methods     
-def ExecuteCommandSubprocess(command, *args):      
+def ExecuteCommandSubprocess(command, *args):
+    print(" ".join((command,)+args))
     try:      
         sp = subprocess.Popen([command, *args], shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)      
         out, err = sp.communicate()    
-        print(" ".join(sp.args)) 
         if out:      
             print(out.decode("utf-8"))      
         if err:      
@@ -63,7 +63,7 @@ fields_list =  (( 'FirstName','First Name*','bold'),
 #UI
 sg.theme('Dark Blue 3')
 
-tabA,tabB,tabC,tabI,tabS =  [] , [] , [], [], []
+tabA,tabB,tabC,tabI,tabS, tabL =  [] , [] , [], [], [], []
 
 #Info
 tabI.extend([
@@ -83,8 +83,6 @@ tabA.extend([
     [sg.Button("Replace",key="replaceBtn")]
 ])
 
-tabA.append([sg.Text('Insert activation data:')])
-
 #Activation Data
 afields = []
 for i in range(len(fields_list)):
@@ -92,12 +90,15 @@ for i in range(len(fields_list)):
         afields.append([
             sg.Text(fields_list[i][1],size=(10, 1),font=('Helvetica', 10, fields_list[i][2])), 
             sg.Combo(COUNTRIES,key=fields_list[i][0],size=(25, 1))
-        ])  
+        ])
+        continue
     afields.append([
         sg.Text(fields_list[i][1],size=(10, 1),font=('Helvetica', 10, fields_list[i][2])), 
         sg.Input(key=fields_list[i][0],size=(25, 1))
     ])
+
 x = len(afields)
+tabB.append([sg.Text('Insert activation data:')])
 tabB.extend([
     [sg.Column(afields[:int(x/2)]),sg.Column(afields[int(x/2):])],
     [sg.Button("Activate",key="activateBtn")]
@@ -109,43 +110,80 @@ tabC.extend([
     [sg.Button('Deactivate',key="deactivateBtn")]
 ])
 
+#Log
+tabL.extend([
+    [sg.Output(size=(70,15))]
+])
+
+# Settings
+tabS.extend([
+    [sg.T("Nvivo Executable Path:")],
+    [sg.Text('Nvivo Path', size=(10, 1)), sg.Input(DEFAULT_NVIVO,key="file_path",size=(50,1)), sg.FileBrowse(key="file")],
+    [sg.T("Proxy Settings")],
+    [sg.Checkbox("Enable Proxy:",default=False, key="proxyT")],
+    [sg.T("Username:",size=(10,1)),sg.I(size=(50,1),key="proxyU")],
+    [sg.T("Password:",size=(10,1)),sg.I(size=(50,1),key="proxyP")],
+    [sg.T("Domain:",size=(10,1)),sg.I(size=(50,1),key="proxyD")],
+])
+
+# Merge all settings
 layout = [  [sg.TabGroup([
                 [   sg.Tab('Info', tabI), 
                     sg.Tab('Replace', tabA), 
                     sg.Tab('Activate', tabB),
                     sg.Tab('Deactivate', tabC),
-                    sg.Tab('Settings', tabS)  ]
+                    sg.Tab('Settings', tabS),
+                    sg.Tab('Log', tabL)  ]
             ])],
-            [sg.Button('Exit',key='exitBtn')]]
+            [sg.Button('Exit',key='exitBtn'),sg.B("print")]]
 
 
-sg.Print('Nvivo Activator', do_not_reroute_stdout=False)
+#sg.Print('Nvivo Activator', do_not_reroute_stdout=False)
 
-window = sg.Window('NVIVO Activation', layout)      
+window = sg.Window('NVIVO Activation', layout)         
+
 
 # ---===--- Loop taking in user input and using it to call scripts --- #      
 
 while True:      
-  (event, values) = window.read()
-  if event == 'exitBtn'  or event is None:      
-      break # exit button clicked    
-      
-  if event == 'replaceBtn':
-    lic = decrypt(values["serial"])
-    #lic = values["serial"]
-    ExecuteCommandSubprocess(NVIVO,"-i",lic)
-    
-  elif event == 'activateBtn':
-    xml = createXML(fields=fields_list,data=values)
-    tempf, fname = mkstemp(text=True)
-    os.close(tempf)
-    
-    with open(fname, 'wb') as f:
-        f.write(b'<?xml version="1.0" encoding="utf-8" standalone="yes"?>')
-        xml.write(f, xml_declaration=False, encoding='utf-8')
-        
-    ExecuteCommandSubprocess(NVIVO,"-a",fname)
-    os.remove(fname)
-    
-  elif event == 'deactivateBtn':
-    ExecuteCommandSubprocess(NVIVO,"-deactivate")
+    (event, values) = window.read()
+    nvivo_path = values["file_path"]
+
+    proxy_settings = []
+
+    if values["proxyT"]:
+        proxy_settings.extend(["-u",values["proxyU"]])
+        if values["proxyP"].replace(" ", "")!="":
+            proxy_settings.extend(["-p",values["proxyP"]])
+        if values["proxyD"].replace(" ", "")!="":
+            proxy_settings.extend(["-d",values["proxyD"]])
+
+    if event == 'exitBtn'  or event is None:      
+        break # exit button clicked    
+
+    if event == 'print':
+        print(nvivo_path)
+        print(proxy_settings)
+
+    if event == 'replaceBtn':
+        lic = decrypt(values["serial"])
+        #lic = values["serial"]
+        cmd = [nvivo_path,"-i",lic]
+        ExecuteCommandSubprocess(nvivo_path,*cmd)
+
+    elif event == 'activateBtn':
+        xml = createXML(fields=fields_list,data=values)
+        tempf, fname = mkstemp(text=True)
+        os.close(tempf)
+
+        with open(fname, 'wb') as f:
+            f.write(b'<?xml version="1.0" encoding="utf-8" standalone="yes"?>')
+            xml.write(f, xml_declaration=False, encoding='utf-8')
+
+        cmd = ["-a",fname] + proxy_settings
+        ExecuteCommandSubprocess(nvivo_path,*cmd)
+        os.remove(fname)
+
+    elif event == 'deactivateBtn':
+        cmd = ["-deactivate"] + proxy_settings
+        ExecuteCommandSubprocess(nvivo_path,*cmd)
