af978fe5549918944a99efef5b47d165785f3977
jimenezcedwin <jimenezcedwin@outlook.com>
2020-06-25 12:22:50 -0500

    Updated Encryption Software

--- Nvivo Activator/crypto.py
+++ Nvivo Activator/crypto.py
@@ -1,41 +1,42 @@
-from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
-from cryptography.hazmat.backends import default_backend
-from cryptography.hazmat.primitives import hashes
 from cryptography.fernet import Fernet
 import PySimpleGUI as sg     
 import base64
 import os
 
-def gen_key_lic(password,salt,msg):
-    kdf = PBKDF2HMAC(
-        algorithm=hashes.SHA256(),
-        length=32,
-        salt=salt.encode(),
-        iterations=100000,
-        backend=default_backend()
-    )
+DIRNAME = ""
+FILENAME = "nvivolicense"
+FERNET_KEY = "O_ZYxyl433xKrZwCG3y7tUEEouZyLJeZmzITsLJ8rzU="
+SERIAL = '11111-22222-33333-44444-55555'
 
-    key = base64.urlsafe_b64encode(kdf.derive(password.encode()))
-    f = Fernet(key)
-    token = f.encrypt(msg.encode())
-    return key.decode(),token.decode()
+COL1 = 13
+COL2 = 50
 
+def savefile(file_path,encrypted_data):
+    with open(file_path, 'wb') as f:
+        f.write(encrypted_data)
 
 layout = [
-    [sg.Text("Message",size=(10,1)),sg.In(key="msg")],
-    [sg.Text("Password",size=(10,1)),sg.In("nvivo12",key="pass")],
-    [sg.Text("Salt",size=(10,1)),sg.In("securesalt",key="salt")],
-    [sg.Button("Generate",key="generateBtn"),sg.Button("Exit")],
-    [sg.Text("Key",size=(10,1)),sg.Output(key="key",size=(50,1))],
-    [sg.Text("License",size=(10,1)),sg.Output(key="output",size=(50,1))]
+    [sg.Text("Serial",size=(COL1,1)),sg.In(SERIAL,size=(COL2,1),key="serial")],
+    [sg.Checkbox('Generate license file', key='check', default=False)],
+    [sg.Text("License Folder",size=(COL1,1)),sg.Input(DIRNAME,key="folder_path",size=(COL2-14,1)), sg.FolderBrowse(key="folder",size=(10,1))],
+    [sg.Text("License Filename",size=(COL1,1)),sg.In(FILENAME,size=(COL2,1))],
+    [sg.Button("New key",key="gkey",size=(COL1,1)),sg.Input(FERNET_KEY,key="keyOutput",size=(COL2,1))],
+    [sg.Text("Encrypted Serial",size=(COL1,1)),sg.Input(key="encSerial",size=(COL2,1))],
+    [sg.Button("Generate",key="generateBtn"),sg.Button("Exit")]
 ]
 
-window = sg.Window('Generate Key',layout)
+window = sg.Window('Encrypt License Key',layout)
 while True:      
     (event, values) = window.read()
     if event == 'Exit'  or event is None:      
         break # exit button clicked   
     if event == "generateBtn":
-        nkey,npass = gen_key_lic(values["pass"],values["salt"],values["msg"])
-        window['key'].update(nkey)
-        window['output'].update(npass)
+        path = DIRNAME+FILENAME+'.lic'
+        fernet = Fernet(FERNET_KEY.encode())
+        encrypted = fernet.encrypt(SERIAL.encode())
+        window['encSerial'].update(encrypted.decode())
+        if values['check']:
+            savefile(path,encrypted)
+    if event == "gkey":
+        FERNET_KEY = Fernet.generate_key().decode()
+        window['keyOutput'].update(FERNET_KEY)
